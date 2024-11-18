3df498ccd84943874760c1d5d1f925e47efdb421
Bahaeddine Saim <bahaeddine4info@gmail.com>
2024-11-18 13:59:06 +0100

    TP2

---
+++ IG5-2/Controllers/FormController.cs
@@ -0,0 +1,21 @@
+﻿using IG5_2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IG5_2.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult create(FormVM form)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return View();
+        }
+    }
+}
---
+++ IG5-2/ViewModels/FormVM.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IG5_2.ViewModels
+{
+    public class FormVM
+    {
+        [Required(ErrorMessage = "Libelle obligatoire")]
+        [RegularExpression("^[A-Za-z0-9 ]+$", ErrorMessage = "Le libelle doit respecter le format requis.")]
+        public string Libelle { get; set; }
+
+        [Required(ErrorMessage = "Code obligatoire")]
+        [MinLength(3, ErrorMessage = "Le code doit contenir au moins 3 caractères.")]
+        public string Code { get; set; }
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Prix obligatoire")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Le prix doit être supérieur à 0.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Choisissez si livraison est disponible.")]
+        public bool Livraison { get; set; }
+
+        [Required(ErrorMessage = "Choisissez un mode de paiement.")]
+        public string ModeDePaiement { get; set; }
+        
+    }
+    }
